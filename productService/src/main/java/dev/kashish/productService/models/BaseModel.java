e773cc292f83a8c51e34698d69ed70299f5b6eb2
user1 <kashishsingh355@gmail.com>
2023-09-18 20:15:03 +0530

    BE: Advanced Database Operations and DB Migrations

--- productService/src/main/java/dev/kashish/productService/models/BaseModel.java
+++ productService/src/main/java/dev/kashish/productService/models/BaseModel.java
@@ -1,10 +1,23 @@
 package dev.kashish.productService.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Setter
 @Getter
+@MappedSuperclass
 public class BaseModel {
-	private long id;
+	@Id
+	@GeneratedValue(generator="uuidgenerator")
+	@GenericGenerator(name="uuidgenerator" , strategy = "uuid2")
+	@Column(name="id" , columnDefinition = "binary(16)" ,nullable = false,updatable = false)
+	private UUID id;
 }
--- productService/src/main/java/dev/kashish/productService/models/Category.java
+++ productService/src/main/java/dev/kashish/productService/models/Category.java
@@ -1,10 +1,15 @@
 package dev.kashish.productService.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class Category extends BaseModel{
+	@Column
 	private String name;
 }
